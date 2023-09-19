2d79111dc4041f5cbc2d13600166fcc22d2357d2
Evgeny <e.tarlo@g.nsu.ru>
2023-09-19 12:04:16 +0700

    task3 upd

--- task03.py
+++ task03.py
@@ -2,3 +2,15 @@
 Задание 3.
 Дан текст. Определить, сколько различных букв в нем.
 '''
+
+text_input = str(input())
+
+text_input = text_input.lower()
+text = list(text_input)
+a = []
+
+for i in range(len(text)):
+    if text[i] not in a:
+        a.append(text[i])
+
+print(len(a))
