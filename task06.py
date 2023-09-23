dce37297a66f59fc1a39991d78ddbb335484ecdb
Evgeny <e.tarlo@g.nsu.ru>
2023-09-23 19:33:18 +0700

    upd6

--- task06.py
+++ task06.py
@@ -2,3 +2,10 @@
 Задание 6.
 Дано предложение. Напечатать его в обратном порядке слов.
 '''
+
+text = str(input())
+text = text.split()
+text.reverse()
+
+for i in range(len(text)):
+    print(text[i], end=' ')
