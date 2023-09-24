93db8a856c94c5b241323a2d6e739a29a780aa39
Evgeny <e.tarlo@g.nsu.ru>
2023-09-24 14:45:35 +0700

    upd7

--- task07.py
+++ task07.py
@@ -2,3 +2,15 @@
 Задание 7.
 Дано предложение. Найти длину его самого короткого слова.
 '''
+
+text = str(input())
+text = text.split()
+
+letter_count = []
+for i in range(len(text)):
+    if text[i].isalpha():
+        letter_count.append(len(text[i]))
+    else:
+        letter_count.append(len(text[i])-1)
+
+print(min(letter_count))
