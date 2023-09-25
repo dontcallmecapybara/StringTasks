b4c0410c6d70185c2579b1edced21daee47f28f0
Evgeny <e.tarlo@g.nsu.ru>
2023-09-25 11:46:48 +0700

    upd11

--- task11.py
+++ task11.py
@@ -7,3 +7,30 @@
 Игрок выигрывает, если он назвал слово последним. Однако, игрок проигрывает, если он
 первым нарушил правила игры.
 '''
+
+
+text = list(map(str, input().lower()))
+
+except_letter = ['ь', 'ъ']
+words = ''
+for letter in range(len(text)):
+    if text[letter].isalpha() and text[letter] not in except_letter:
+        words += text[letter]
+    elif text[letter] == ' ':
+        words += ' '
+
+counter = 1
+words = list(words)
+for i in range(len(words)-2):
+    if words[i].isalpha() and words[i+1] == ' ' and words[i+2].isalpha():
+        if words[i] == words[i+2]:
+            counter += 1
+        else:
+            pass
+    else:
+         pass
+
+if counter % 2 == 0:
+    print('Winner is Vasya')
+else:
+    print('Winner is Petya')
