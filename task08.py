2b7ac8e5ec94df73e8aa0836c66d971f2d047bf0
Evgeny <e.tarlo@g.nsu.ru>
2023-09-25 10:52:51 +0700

    upd8

--- task08.py
+++ task08.py
@@ -2,3 +2,19 @@
 Задание 8.
 Дано предложение. Напечатать все его слова в порядке неубывания их длин.
 '''
+
+text = list(input())
+
+new_text = []
+for i in range(len(text)):
+    if text[i].isalpha():
+        new_text.append(text[i])
+    elif text[i] == ' ':
+        new_text.append(' ')
+
+text = ''
+for j in range(len(new_text)):
+    text += new_text[j]
+
+sorted_text = sorted(text.split(), key=lambda word: len(word))
+print(sorted_text)
--- task09.py
+++ task09.py
@@ -2,3 +2,23 @@
 Задание 9.
 Дано предложение. В нем только два слова одинаковые. Найти эти слова.
 '''
+
+
+def find_duplicate_words(text):
+    words = text.lower().split()
+    duplicates = []
+    for word in words:
+        if words.count(word) > 1 and word not in duplicates:
+            duplicates.append(word)
+    return duplicates
+
+
+text = str(input())
+words = text.lower().split()
+
+copies = []
+for i in words:
+    if words.count(i) == 2 and i not in copies:
+        copies.append(i)
+
+print(copies)
