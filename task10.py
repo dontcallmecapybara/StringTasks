ff7d79fba238f1d8c88c7003d02f6b266e76bf5d
Evgeny <e.tarlo@g.nsu.ru>
2023-09-25 10:54:32 +0700

    upd10

--- task10.py
+++ task10.py
@@ -1,5 +1,36 @@
 '''
 Задание 10.
-Дано предложение. Выведите на экран те слова, которые отличны от первого слова,
-в слове нет повторяющихся букв и это слово симметрично.
+Дано предложение. Выведите на экран те слова, которые отличны от первого слова
+и в слове нет повторяющихся букв.
 '''
+
+text = list(input().lower())
+
+new_text = []
+for i in range(len(text)):
+    if text[i].isalpha():
+        new_text.append(text[i])
+    elif text[i] == ' ':
+        new_text.append(' ')
+
+text = ''
+for j in range(len(new_text)):
+    text += new_text[j]
+text = text.split()
+
+first_word = text[0]
+del text[0]
+for duplicate_word in range(len(text)-1):
+    if text[duplicate_word] == first_word:
+        del text[duplicate_word]
+    else:
+        pass
+
+finding_words = []
+for word in range(len(text)):
+    temp_list = list(text[word])
+    unique_words = set(temp_list)
+    if len(temp_list) == len(unique_words):
+        finding_words.append(text[word])
+
+print(finding_words)
