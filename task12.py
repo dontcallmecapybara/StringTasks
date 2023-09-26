bba272471e3c4569acb7ac3a3017907539bfb44c
Evgeny <e.tarlo@g.nsu.ru>
2023-09-26 10:09:37 +0700

    upd12

--- task12.py
+++ task12.py
@@ -5,3 +5,32 @@
 ключевым словом. Напишите программу, которая проверяет введенную строку, может ли
 она быть именем в языке Python.
 '''
+
+import keyword
+
+name = input()
+
+upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+lower = "abcdefghijklmnopqrstuvwxyz"
+numbers = "0123456789"
+special = "_"
+characters = list(upper + lower + numbers + special)
+
+valid_name = True
+
+if keyword.iskeyword(name):
+    valid_name = False
+else:
+    name = list(name)
+    if name[0] in numbers:
+        valid_name = False
+    else:
+        for i in list(name):
+            if i not in characters:
+                valid_name = False
+                break
+
+if valid_name:
+    print('The string can be a name.')
+else:
+    print('The string cannot be a name.')
